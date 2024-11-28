31c56297f6c284298c867329b941b0630da6c95c
Sergii Tykhomyrov <sergii@tykhomyrov.net>
2024-11-28 06:48:26 +0100

    Import NeoVim configuration

---
+++ nvim/dot-config/nvim/init.lua
@@ -0,0 +1,28 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+vim.g.mapleader = " "
+
+vim.keymap.set("n", "<C-Tab>", "<C-w>w")
+
+require("lazy").setup("plugins")
+
+require("lspconfig").pyright.setup({})
+require("lspconfig").ruby_lsp.setup({})
