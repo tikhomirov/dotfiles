7d37761e35d20e9495d962718099b3526f6324b8
Sergii Tykhomyrov <sergii@tykhomyrov.net>
2024-12-01 20:25:15 +0100

    Improve LSP configuration

---
+++ nvim/dot-config/nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "pyright", "ruby_lsp" }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.pyright.setup({})
+      lspconfig.ruby_lsp.setup({})
+    end
+  }
+}
