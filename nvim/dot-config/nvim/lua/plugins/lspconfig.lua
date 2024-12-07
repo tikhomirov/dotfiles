ca19d8fc4d186221d06ec470e0d09a6208f4fe09
Sergii Tykhomyrov <sergii@tykhomyrov.net>
2024-12-07 18:22:38 +0100

    nvim: Add key mappings for LSP actions

--- nvim/dot-config/nvim/lua/plugins/lspconfig.lua
+++ nvim/dot-config/nvim/lua/plugins/lspconfig.lua
@@ -18,8 +18,16 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
+
       lspconfig.pyright.setup({})
       lspconfig.ruby_lsp.setup({})
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
     end
   }
 }
