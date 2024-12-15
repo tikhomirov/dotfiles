09ccb63b74894a39b041ac159452b4f970083c39
Sergii Tykhomyrov <sergii@tykhomyrov.net>
2024-12-15 19:08:38 +0100

    nvim: Update indent settings and disable relative numbers

--- nvim/dot-config/nvim/init.lua
+++ nvim/dot-config/nvim/init.lua
@@ -15,10 +15,11 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
-vim.opt.relativenumber = true
 vim.opt.number = true
 vim.opt.swapfile = false
 vim.opt.spell = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
 
 vim.g.mapleader = " "
 
